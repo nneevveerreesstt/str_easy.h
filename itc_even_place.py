351b66d8979753d19c84d9d0859d68ef51e953f6
nneevveerreesstt <120297377+nneevveerreesstt@users.noreply.github.com>
2022-12-18 21:46:46 +0300

    Update itc_even_place.py

--- itc_even_place.py
+++ itc_even_place.py
@@ -1,5 +1,10 @@
-def itc_even_place(string):
-    if len(string) <= 1:
-        return -1
-    else:
-        return string[1::2]
+def itc_first_end_three(string):
+    ln = 0
+    new_str = ""
+    for i in string:
+        ln += 1
+    if ln <= 1:
+        return "-1"
+    for i in range(1, ln, 2):
+        new_str += string[i]
+    return new_str
