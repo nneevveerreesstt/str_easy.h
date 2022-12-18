123e3e89d2b411c1f9d967668a8d49de73e94db8
nneevveerreesstt <120297377+nneevveerreesstt@users.noreply.github.com>
2022-12-18 22:42:21 +0300

    Update itc_equal_reverse.py

--- itc_equal_reverse.py
+++ itc_equal_reverse.py
@@ -1,6 +1,9 @@
 def itc_equal_reverse(string):
     res = ""
-    for i in range(len(string) - 1, -1, -1):
+    ln = 0
+    for i in string:
+        ln += 1
+    for i in range(ln - 1, -1, -1):
         res += string[i]
     if string == res:
         return True
