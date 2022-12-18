2df03093b2d85bb05e78ce9cf6d6610d018bf0d0
nneevveerreesstt <120297377+nneevveerreesstt@users.noreply.github.com>
2022-12-18 21:38:28 +0300

    Update itc_first_end_three.py

--- itc_first_end_three.py
+++ itc_first_end_three.py
@@ -1,5 +1,9 @@
 def itc_first_end_three(string):
-    if len(string)<5:
-        return string[0] * len(string)
+    ln = 0
+    for i in string:
+        ln += 1
+    if ln > 5:
+        print(string[0] + string[1] + string[2] + string[ln-3] + string[ln-2] + string[ln-1])
     else:
-        return string[:3] + string[len(string)-3:]
+        for i in range(ln):
+            print(string[0], end="")
