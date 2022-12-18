4e10382af58b48cac3e530c2a88974fecc9428d2
nneevveerreesstt <120297377+nneevveerreesstt@users.noreply.github.com>
2022-12-18 22:27:23 +0300

    Update itc_reverse_str.py

--- itc_reverse_str.py
+++ itc_reverse_str.py
@@ -1,5 +1,8 @@
 def itc_reverse_str(string):
+    ln = 0
+    for i in string:
+        ln += 1
     res = ""
-    for i in range(len(string) - 1, -1, -1):
+    for i in range(ln - 1, -1, -1):
         res += string[i]
     return res
